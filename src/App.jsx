da61c2d3d5e99d88af49aee16091efa6d3f4b3c2
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-18 18:58:30 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="success" as="button">
+        Success
+      </Button>
+    </div>
+  );
+};
+
+export default App;
