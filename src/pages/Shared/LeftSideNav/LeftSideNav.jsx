489c5b8ce9732f9f46d197fdb534b4b27c0d92c3
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-18 21:06:56 +0600

    added categories

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-  return <div>LeftSideNav</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/news-categories`)
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <div className="">
+        <h4>Total categories: {categories.length} </h4>
+        <div className="">
+          {categories
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map(category => (
+              <p key={category.id}>
+                <Link to={`/category/${category.id}`}>{category.name}</Link>
+              </p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
